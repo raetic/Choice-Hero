9e46bc1535284c397b50bc5cb04ba01f10f8181d
raetic <83604180+raetic@users.noreply.github.com>
2022-09-06 16:20:19 +0900

    0906 오브젝트 풀링

--- Assets/Script/Battle/Body.cs
+++ Assets/Script/Battle/Body.cs
@@ -44,7 +44,7 @@ public class Body : MonoBehaviour
         {
 
             myPlayer.ExpUp(collision.gameObject.GetComponent<Exp>().mount);
-            Destroy(collision.gameObject);
+            ExpController.instance.ReturnObjectToQueue(collision.gameObject.GetComponent<Exp>());
         }
         if (collision.gameObject.tag == "Ax")
         {
